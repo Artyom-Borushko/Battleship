076cbb9fbe91df66ad007448b1c32984c94d5095
Artyom Borushko <artsem_barushka@epam.com>
2021-12-19 20:59:49 +0300

    add welcome screen & battleship greed

---
+++ src/js/battleshipGrid.js
@@ -0,0 +1,28 @@
+const cellHeaders = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'M']
+const battleshipGrid = document.querySelector('.battleship-panel');
+
+export function createBattleshipGrid() {
+  for (let column = 0; column <= 10; column++) {
+    const columnDiv = document.createElement('div');
+    columnDiv.classList.add(`column-${column}`);
+    columnDiv.classList.add('battleship-column');
+    for (let row = 0; row < 11; row++) {
+      const cellDiv = document.createElement('div');
+      cellDiv.classList.add(`cell-${row}`);
+      cellDiv.classList.add('battleship-cell');
+      columnDiv.appendChild(cellDiv);
+      if (column === 0 && row > 0) {
+        const rowText = document.createTextNode(row);
+        cellDiv.appendChild(rowText);
+      }
+      if (column > 0 && row === 0) {
+        const cellText = document.createTextNode(cellHeaders[column - 1]);
+        cellDiv.appendChild(cellText);
+      }
+      if (column > 0 && row > 0) {
+        cellDiv.classList.add('battleship-cell-playable');
+      }
+    }
+    battleshipGrid.appendChild(columnDiv);
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+import { createBattleshipGrid } from "./battleshipGrid.js";
+
+const loginFormSelector = document.querySelector('#login-form');
+const nameInputSelector = document.querySelector('.name-input');
+const startButtonSelector = document.querySelector('.start-button');
+const gameScreenSelector = document.querySelector('.game-screen');
+
+
+loginFormSelector.addEventListener('submit', onLoginFormSubmit);
+nameInputSelector.addEventListener('keyup', loginButtonEnabler);
+
+
+function loginButtonEnabler(event) {
+  if (event.key) {
+    startButtonSelector.style.cursor = "pointer"
+    startButtonSelector.disabled = false;
+  }
+  else {
+    startButtonSelector.disabled = true;
+  }
+}
+
+function onLoginFormSubmit(event) {
+  event.preventDefault();
+  loginFormSelector.style.display = "none";
+  gameScreenSelector.style.display = "block";  
+  createBattleshipGrid();
+  // TODO - add user name to storage
+
+}
+
+
+
